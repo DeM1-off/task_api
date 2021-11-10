4d4928d19cc726976f7ea9e509a2a95317f88322
Dmitro Dohryk <52733818+DeM1-off@users.noreply.github.com>
2021-11-10 18:36:56 +0200

    Updated migration

--- database/migrations/2021_11_01_075654_create_companies_table.php
+++ database/migrations/2021_11_01_075654_create_companies_table.php
@@ -22,8 +22,7 @@ class CreateCompaniesTable extends Migration
                 $table->string('description');
                 $table->unsignedBigInteger('user_id');
                 $table->timestamps();
-    
-                $table->foreign('id')->references('user_id')->on('users');
+                $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             });
 		}
     }
